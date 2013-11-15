8ec38249686c9a11cdad802a184817334b518a5d
Christoph Burschka <christoph@burschka.de>
2013-11-15 13:47:30 +0000

    pnf-Hauptfunktion

--- src/modod/__init__.py
+++ src/modod/__init__.py
@@ -5,9 +5,13 @@ parser = grammar_extended.build_grammar().slr1()
 
 def parse(string):
     return parser.parse(lexer.lex(string)).dre()
-    
+
 def nnf(dre):
     return dre.nary_normal_form()
-    
+
+def pnf(dre):
+    # nnf(p▴(p•(x)))
+    return nnf(dre._pnf1()._pnf3())
+
 def write(dre):
     return dre.formula()
