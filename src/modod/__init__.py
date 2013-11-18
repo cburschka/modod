2351b08701d86665bdb125c5113b7440164fa415
Christoph Burschka <christoph@burschka.de>
2013-11-18 12:26:58 +0000

    Bugfix: Der globale __init__-Namespace überschreibt Modulnamen.

--- src/modod/__init__.py
+++ src/modod/__init__.py
@@ -1,10 +1,10 @@
 from . import grammar_extended
 
-lexer = grammar_extended.build_lexer()
-parser = grammar_extended.build_grammar().slr1()
+_lexer = grammar_extended.build_lexer()
+_parser = grammar_extended.build_grammar().slr1()
 
 def parse(string):
-    return parser.parse(lexer.lex(string)).dre()
+    return _parser.parse(_lexer.lex(string)).dre()
 
 def nnf(dre):
     return dre.nary_normal_form()
