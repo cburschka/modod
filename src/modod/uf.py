6499e97305475dd1de4ebf3bf1d69b530dce4fcd
Christoph Burschka <christoph@burschka.de>
2013-12-04 14:19:58 +0000

    Erlaube Erzeugung eines leeren UF; verbessere den Test

--- src/modod/uf.py
+++ src/modod/uf.py
@@ -1,10 +1,10 @@
 class UF:
-    def __init__(self, iterable):
+    def __init__(self, iterable=()):
         self.list = list(iterable)
         self.index = {x:i for i,x in enumerate(self.list)}
         self.parent = list(range(len(self.list)))
 
-    def add(self, x):
+    def makeset(self, x):
         self.index[x] = len(self.list)
         self.parent.append(len(self.list))
         self.list.append(x)
--- src/test_uf.py
+++ src/test_uf.py
@@ -1,11 +1,18 @@
 import modod.uf as uf
-x = uf.UF(map(chr, range(65, 75)))
+x = uf.UF(map(chr, range(65, 70)))
+print("Initialize UF with A ... E : ", x)
+for y in map(chr, range(70, 75)):
+    x.makeset(y)
+    print("Add ", y, " : ", x)
 print(x)
-x.union('A','B')
-print(x)
-x.union('C','D')
-x.union('E','F')
-x.union('G','H')
-print(x)
-x.union('C','H')
-print(x)
+
+unions = [
+    ('A', 'B'), ('C', 'E'), ('G', 'J'),
+    ('B', 'F'), ('G', 'H'), ('I', 'A'),
+    ('D', 'C'), ('H', 'E'), ('A', 'E')
+]
+
+for a,b in unions:
+    x.union(a, b)
+    print("Uniting ",a,", ",b," : ",x)
+    
