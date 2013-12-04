e31221d77b208142b03413ff7953cf54af0f52be
Dominik Freydenberger <freydenberger@586dbb46-b040-41f0-b297-2b851647851d>
2013-12-04 09:50:12 +0000

    mehr unittests

--- src/unittests.py
+++ src/unittests.py
@@ -37,19 +37,43 @@ class TestUM(unittest.TestCase):
 		ist='(((a1? | b1?)? |(c1? | d1?)?)?|((a2? | b2?)? |(c2? | d2?)?)?)?'
 		self.assertEqual(DREfromString(ist).toNNF().toPNF().toString(),'(a1|b1|c1|d1|a2|b2|c2|d2)?')
 
+	def test_pnf_no_side_effects(self):
+		rx='(((a1? | b1?)? |(c1? | d1?)?)?|((a2? | b2?)? |(c2? | d2?)?)?)?'
+		pnfrx=DREfromString(rx).toNNF().toPNF()
+		self.assertEqual(pnfrx.toString(),'(a1|b1|c1|d1|a2|b2|c2|d2)?')
+		self.assertEqual(rx,'(((a1? | b1?)? |(c1? | d1?)?)?|((a2? | b2?)? |(c2? | d2?)?)?)?')
+
+	def test_determinism_1(self):
+		rxstr = 'a+'
+		#self.assertTrue(rxstr.isDeterministic())
+		A = OccurrenceAutomaton(import_dre.add_index(DREfromString(rxstr)))
+		self.assertTrue(A.isDeterministic())
+
+	def test_determinism_2(self):
+		rxstr = '(a)*a'
+		#self.assertFalse(rxstr.isDeterministic())
+		A = OccurrenceAutomaton(import_dre.add_index(DREfromString(rxstr)))
+		self.assertFalse(A.isDeterministic())
+
+	def test_determinism_2(self):
+		rxstr = '(ab)|(ac)'
+		#self.assertFalse(rxstr.isDeterministic())
+		A = OccurrenceAutomaton(import_dre.add_index(DREfromString(rxstr)))
+		self.assertFalse(A.isDeterministic())
+		
 	# def test_eq_1(self):
 	# 	rxA = DREfromString('a+')
 	# 	rxB = DREfromString('a*')
 	# 	A = OccurrenceAutomaton(import_dre.add_index(rxA))
 	# 	B = OccurrenceAutomaton(import_dre.add_index(rxB))
-	# 	self.assertEqual(equivalentTo(A,A),True)
-	# 	self.assertEqual(equivalentTo(B,B),True)
-	# 	self.assertEqual(equivalentToMEW(A,A),True)
-	# 	self.assertEqual(equivalentToMEW(B,B),True)
-	# 	self.assertEqual(equivalentToMEW(A,B),True)
-	# 	self.assertEqual(equivalentToMEW(B,A),True)
-	# 	self.assertEqual(equivalentTo(B,A),False)
-	# 	self.assertEqual(equivalentTo(A,B),False)
+	# 	self.assertTrue(equivalentTo(A,A))
+	# 	self.assertTrue(equivalentTo(B,B))
+	# 	self.assertTrue(equivalentToMEW(A,A))
+	# 	self.assertTrue(equivalentToMEW(B,B))
+	# 	self.assertTrue(equivalentToMEW(A,B))
+	# 	self.assertTrue(equivalentToMEW(B,A))
+	# 	self.assertFalse(equivalentTo(B,A))
+	# 	self.assertFalse(equivalentTo(A,B))
  
 	# def test_eq_2(self):
 	# 	rxA = DREfromString('(((a1?|b1?)?|(c1?|d1?)?)?,((a2?|b2?)?|(c2?|d2?)?)?)+')
@@ -58,21 +82,21 @@ class TestUM(unittest.TestCase):
 	# 	A = OccurrenceAutomaton(import_dre.add_index(rxA))
 	# 	B = OccurrenceAutomaton(import_dre.add_index(rxB))
 	# 	C = OccurrenceAutomaton(import_dre.add_index(rxC))	
-	# 	self.assertEqual(equivalentTo(A,A),True)
-	# 	self.assertEqual(equivalentTo(B,B),True)
-	# 	self.assertEqual(equivalentTo(C,C),True)	
-	# 	self.assertEqual(equivalentToMEW(A,B),True)
-	# 	self.assertEqual(equivalentToMEW(B,A),True)
-	# 	self.assertEqual(equivalentToMEW(B,C),True)
-	# 	self.assertEqual(equivalentToMEW(C,B),True)
-	# 	self.assertEqual(equivalentToMEW(C,A),True)
-	# 	self.assertEqual(equivalentToMEW(A,C),True)
-	# 	self.assertEqual(equivalentTo(A,B),True)
-	# 	self.assertEqual(equivalentTo(B,A),True)
-	# 	self.assertEqual(equivalentTo(B,C),False)
-	# 	self.assertEqual(equivalentTo(C,B),False)
-	# 	self.assertEqual(equivalentTo(C,A),False)
-	# 	self.assertEqual(equivalentTo(A,C),False)
+	# 	self.assertTrue(equivalentTo(A,A))
+	# 	self.assertTrue(equivalentTo(B,B))
+	# 	self.assertTrue(equivalentTo(C,C))	
+	# 	self.assertTrue(equivalentToMEW(A,B))
+	# 	self.assertTrue(equivalentToMEW(B,A))
+	# 	self.assertTrue(equivalentToMEW(B,C))
+	# 	self.assertTrue(equivalentToMEW(C,B))
+	# 	self.assertTrue(equivalentToMEW(C,A))
+	# 	self.assertTrue(equivalentToMEW(A,C))
+	# 	self.assertTrue(equivalentTo(A,B))
+	# 	self.assertTrue(equivalentTo(B,A))
+	# 	self.assertFalse(equivalentTo(B,C))
+	# 	self.assertFalse(equivalentTo(C,B))
+	# 	self.assertFalse(equivalentTo(C,A))
+	# 	self.assertFalse(equivalentTo(A,C))
  
 if __name__ == '__main__':
 	unittest.main()
