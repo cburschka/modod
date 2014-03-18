fc5945cd2bcfdb615aff7e08c4d23f0dadb16260
Dominik Freydenberger <freydenberger@586dbb46-b040-41f0-b297-2b851647851d>
2014-03-18 11:00:18 +0000

    dtd nicer

---
+++ README-dtd.txt
@@ -0,0 +1,47 @@
+usage: dtd.py [-h] [-a [AUTPREFIX]] [-c] [-co] [-d]
+              [-e ELEMENTS [ELEMENTS ...]] [-j] [-n] [-s] [-u] [-we] [-wep]
+              [-wes]
+              files [files ...]
+
+This tool takes a list of XMLfiles and computes a DTD.
+
+positional arguments:
+  files                 the XML file(s) from which the element type
+                        declarations are to be inferred
+
+optional arguments for regular users:
+  -h, --help            show this help message and exit
+  -c, --chare           infer a chain regular expression, instead of a single
+                        occurrence regular expression (the former are flatter
+                        than the latter)
+  -d, --dre             write output as deterministic regular expression,
+                        instead of an element type declaration (also activates
+                        -j)
+  -e ELEMENTS [ELEMENTS ...], --elements ELEMENTS [ELEMENTS ...]
+                        determines for which element names an element type
+                        declaration is inferred
+  -j, --just-elements   do not put the DOCTYPE declaration around the element
+                        tags
+  -s, --skip-empty      do not display declarations of elements that have no
+                        childer
+  -u, --ugly            do not use prettification algorithm
+
+Default is inferring an element type definition for every element in the
+files. If you want to compute this for only some elements, use the -e flag. If
+you want to exclude elements that have empty definitions, use the -s flag. 
+
+optional arguments for users who want to run tests or care about the theory:
+-a [AUTPREFIX], --automaton [AUTPREFIX]
+                       for every element E, the inferred SOA is written to
+                       the file AUTPREFIX E.dot in the dot-format of Graphviz
+-co, --counts          display how often elements occur
+-n, --no-inference     do not infer element type declarations (only useful if
+                       -a is used as well)
+-t, --time-stamps      includes some timestamps (for very elementary profiling) 
+-v, --verbose          print additional information
+-we, --write-elements  for every element E, write the inferred DTD/regular
+                       expression to a file WPREFIX E.WSUFFIX (definable by
+                       -wp,-ws)
+-wep, --write-prefix  sets WPREFIXe (for -we), default empty
+-wes, --write-suffix  sets WPREFIXe (for -we), default .dtd
+
--- src/dtd-inf/dtd.py
+++ src/dtd-inf/dtd.py
@@ -11,26 +11,36 @@ import modod
 from modod.soa import SingleOccurrenceAutomaton
 
 
-parser = argparse.ArgumentParser(description="This tool takes a list of XMLfiles and computes a DTD.",epilog="Default is inferring an element type definition for every element in the files. If you want to compute this for only some elements, use the -e flag. If you want to exclude elements that have empty definitions, use the -s flag. If you just want a DTD and do not care about the theoretic background of this tool, you should ignore the flags  -a, -co, -n, -t, -v, -we, -wep, and -wes.")
+parser = argparse.ArgumentParser(description="This tool takes a list of XMLfiles and computes a DTD.",epilog="Default is inferring an element type definition for every element in the files. If you want to compute this for only some elements, use the -e flag. If you want to exclude elements that have empty definitions, use the -s flag. See the readme file for hidden arguments.")
 parser.add_argument("files",help="the XML file(s) from which the element type declarations are to be inferred",nargs="+")
 
-parser.add_argument("-a","--automaton",help="for every element E, the inferred SOA is written to the file AUTPREFIX E.dot in the dot-format of Graphviz",dest="autprefix",type=str,nargs="?",default="")
+parser.add_argument("-a","--automaton",help=argparse.SUPPRESS,dest="autprefix",type=str,nargs="?",default="")
 parser.add_argument("-c","--chare",help="infer a chain regular expression, instead of a single occurrence regular expression (the former are flatter than the latter)",action="store_true")
-parser.add_argument("-co","--counts",help="disply how often elements occur",dest="counts",action="store_true")
+parser.add_argument("-co","--counts",help=argparse.SUPPRESS,dest="counts",action="store_true")
 parser.add_argument("-d","--dre",help="write output as deterministic regular expression, instead of an element type declaration (also activates -j)",action="store_true")
 parser.add_argument("-e","--elements",help="determines for which element names an element type declaration is inferred",dest="elements",nargs="+",default=[])
-#parser.add_argument("-f","--force",help="necessary if no list of elements is provided",action="store_true")
 parser.add_argument("-j","--just-elements",help="do not put the DOCTYPE declaration around the element tags",dest="justelements",action="store_true")
-parser.add_argument("-n","--no-inference",help="do not infer element type declarations (only useful if -a is used as well)",dest="noinference",action="store_true")
+parser.add_argument("-n","--no-inference",help=argparse.SUPPRESS,dest="noinference",action="store_true")
 parser.add_argument("-s","--skip-empty",help="do not display declarations of elements that have no childer",dest="skipempty",action="store_true")
 parser.add_argument("-t","--timestamps",help=argparse.SUPPRESS,action="store_true")
 parser.add_argument("-u","--ugly",help="do not use prettification algorithm",action="store_true")
 parser.add_argument("-v","--verbose",help=argparse.SUPPRESS,dest="verbose",action="store_true")
-# parser.add_argument("-t","--timestamps",help="show timestamps for important tasks",action="store_true")
+parser.add_argument("-we","--write-elements",help=argparse.SUPPRESS,action="store_true",dest="writeElements")
+parser.add_argument("-wep","--write-prefix",help=argparse.SUPPRESS,action="store_true",dest="writeprefix",default="")
+parser.add_argument("-wes","--write-suffix",help=argparse.SUPPRESS,action="store_true",dest="writesuffix",default=".dtd")
+
+#parser.add_argument("-a","--automaton",help="for every element E, the inferred SOA is written to the file AUTPREFIX E.dot in the dot-format of Graphviz",dest="autprefix",type=str,nargs="?",default="")
+#parser.add_argument("-co","--counts",help="display how often elements occur",dest="counts",action="store_true")
+#parser.add_argument("-n","--no-inference",help="do not infer element type declarations (only useful if -a is used as well)",dest="noinference",action="store_true")
+#parser.add_argument("-t","--timestamps",help="includes some timestamps (for very elementary profiling) ",action="store_true")
 #parser.add_argument("-v","--verbose",help="print additional information",dest="verbose",action="store_true")
-parser.add_argument("-we","--write-elements",help="for every element E, write the inferred DTD/regular expression to a file WPREFIX E.WSUFFIX (definable by -wp,-ws)",action="store_true",dest="writeElements")
-parser.add_argument("-wep","--write-prefix",help="sets WPREFIXe (for -we), default empty",action="store_true",dest="writeprefix",default="")
-parser.add_argument("-wes","--write-suffix",help="sets WPREFIXe (for -we), default .dtd",action="store_true",dest="writesuffix",default=".dtd")
+#parser.add_argument("-we","--write-elements",help="for every element E, write the inferred DTD/regular expression to a file WPREFIX E.WSUFFIX (definable by -wp,-ws)",action="store_true",dest="writeElements")
+#parser.add_argument("-wep","--write-prefix",help="sets WPREFIXe (for -we), default empty",action="store_true",dest="writeprefix",default="")
+#parser.add_argument("-wes","--write-suffix",help="sets WPREFIXe (for -we), default .dtd",action="store_true",dest="writesuffix",default=".dtd")
+
+
+#parser.add_argument("-f","--force",help="necessary if no list of elements is provided",action="store_true")
+
 
 args=parser.parse_args()
 
